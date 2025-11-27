00832a6b5c4ba062cc5bb48c1b18116d889d9bad
bhakthisp <bhakthispgowda13@gmail.com>
2025-11-27 18:53:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+$(document).ready(function () {
+    // Alert on form submit
+    $("#regForm").submit(function () {
+        alert("Form submitted successfully!");
+        return true; // continue submission
+    });
+
+    // Optional: fade-in the container
+    $(".container").hide().fadeIn(600);
+});
 
